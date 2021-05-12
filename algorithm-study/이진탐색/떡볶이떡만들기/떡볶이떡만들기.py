d4fd211d32906d763106e159d3b38bca4296bd47
Guest <k0912h@naver.com>
2021-05-12 22:34:57 +0900

    binary search modify

--- algorithm-study/이진탐색/떡볶이떡만들기/떡볶이떡만들기.py
+++ algorithm-study/이진탐색/떡볶이떡만들기/떡볶이떡만들기.py
@@ -2,13 +2,23 @@ N, M = map(int, input().split())
 ricecake = list(map(int, input().split()))
 
 big = max(ricecake)
-temp = [i for i in range(big+1)]
 
 height = []
 
 def findM(array, length):
-    sum = 0
-    for i in array:
-        if length < i:
-            sum += (i-length)
-    return sum
+	s = 0
+	for i in array:
+		if length < i:
+			s += (i-length)
+	return s
+
+def findmax(ricecake, start, end):
+	while start<=end:
+		mid = (start+end) // 2
+		if findM(ricecake, mid) >= M:
+			height.append(mid)
+			start = mid + 1
+		else:
+			end = mid - 1
+findmax(ricecake, 0,  big)
+print(height[-1])
